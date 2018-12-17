862ff85b431db4b472e9e54cb611cb3017104c6e
Mike Lu <heatmint2333@gmail.com>
2018-12-17 15:22:25 +0800

    Batter board

--- src/static/script.js
+++ src/static/script.js
@@ -1,8 +1,13 @@
 window.onload = function () {
-    var y = window.screen.height*0.8;
+
+    var y = window.screen.height>=window.screen.width?window.screen.width*0.8:window.screen.height*0.8;
     var gamey = Math.floor(y);
     var border = Math.round(gamey/20);
     var between = Math.floor((gamey-2*border)/14);
+
+    function converter(x) {
+        return (x)*between+border;
+    }
 
     var board = document.getElementById('board');
     var canvas = document.createElement('canvas');
@@ -14,11 +19,24 @@ window.onload = function () {
 
     for(i=0;i<15;i++){
         ext.moveTo(border+(i*between),border);
-        ext.lineTo(border+(i*between),gamey-border);
+        ext.lineTo(border+(i*between),border+(14*between));
         ext.stroke();
         ext.moveTo(border,border+(i*between));
-        ext.lineTo(gamey-border,border+(i*between));
+        ext.lineTo(border+(14*between),border+(i*between));
         ext.stroke();
     }
+
+    function drawc(x,y){
+        ext.beginPath();
+        ext.arc(converter(x), converter(y),Math.floor(between/5),0,2*Math.PI);
+        ext.fill();
+        ext.closePath();
+    }
+
+    drawc(7,7);
+    drawc(3,3);
+    drawc(11,3);
+    drawc(3,11);
+    drawc(11,11);
 
 };
